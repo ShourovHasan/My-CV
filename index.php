379a20f82e99b0cf0bcfc2b65229ffafdfe166b1
Shourov Hasan <39989541+ShourovHasan@users.noreply.github.com>
2018-07-08 12:53:50 +0600

    Index file uploaded
    uploaded by SS

---
+++ index.php
@@ -0,0 +1,79 @@
+<!Doctype>
+<html>
+		<head>
+			<title>My CV </title>
+		</head>
+	<body>
+		<table border ="4px" cellpadding="4" align="center" width ="60%" style="background-color:grey" style="font-color:grey">
+			<tr style="background-color:white;">
+				<th>
+					<h1 style="color:Tomato;"> Welcome To My CV <h1>
+				</th>
+			</tr>
+			<tr>
+				<th align ="left"><h2 style="color:white;">HASAN,MD SHOUROV </h2>
+					<img src="Images/Shourov.jpg" align="right" style="width:165px;height:220px;"><br>
+					<b> Work:Student </b><br>
+					Phone No: 01786558637<br>
+					Mail:<a href="mailto:name@gmail.com">shourovhasan91@gmail.com</a><br><br>
+
+					<h3> Personal Information:<h3>
+						<p>
+							<ul>
+								<li>Father Name: Abdul Khalek Bepari </li>
+								<li>Religion: ISLAM </li>
+								<li>NID: 9156482387412563 </li>
+								<li>Nationality: Bangladeshi </li>
+								<li>Marital Status: Single </li>
+							</ul>
+						</p>
+					<h3>Objective</h3>
+						<p>
+							<ul>
+								<li>Currently going  through Computer Science and Engineering.</li>
+								<li>Learn basic:HTML,CSS,PHP,XML,Java-Script</li>
+								<li>Good Communication Skills, Presentation Skills.</li>
+							</ul>
+						</p>
+					<h2>Educational Qualification:</h2>
+					<table border ="2" cellpadding="2" style="width:70%" align="center">
+						<tr>
+							<th>Examination</th>
+							<th>Division</th>
+							<th>Board/University </th>
+							<th>Year </th>
+						</tr>
+						<tr><td>SSC(SCIENCE) </td><td> 1st </td><td>DHAKA BOARD </td><td>2012 </td></tr>
+						<tr><td>HSC(SCIENCE) </td><td> 1st </td><td>DHAKA BOARD </td><td>2014 </td></tr>
+						<tr><td>BSc(CS) </td><td> 1st </td><td> AIUB </td><td>Processing </td></tr>
+					</table>
+					<h2>Personal Skills:</h2>
+						<p>
+							<ul>
+								<li>Ms word,Excel,Power Point </li>
+								<li>Seo,web design </li>
+								<li>Inter marketing,video editing </li>
+							</ul>
+						</p>
+						
+						<p>
+							<h3>Reference:</h3>
+						   <ul>
+								<li>MD Badol Ahmed<br>
+									Goalundo Kamrul Islam Digree college.<br>
+									DHAKA.<br>
+									Relation:Principal
+								</li>
+								<li>
+									MD Sirajul Islam<br>
+									Suraj Mohini Institute<br>
+									DHAKA<br>
+									Relation:Principal
+								</li>
+							</ul>
+						</p>
+				</th>
+			</tr>
+		</table>
+	</body>
+</html>
